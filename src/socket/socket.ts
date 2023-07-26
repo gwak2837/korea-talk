7eb4244cab6f741c7439cfa4a525cef8e6a0da39
Taeuk Gwak <gwak2837@kakao.com>
2023-07-26 11:35:32 +0900

    Update: text

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client'
 
 // 현재 파일은 수정하지 않고 사용만 해주세요.
 
-const serverUrl = 'ws://front-assignment.exp.channel.io'
+const serverUrl = 'wss://front-assignment.exp.channel.io'
 
 class Socket {
   handlers = new Set<(email) => void>()
@@ -10,8 +10,8 @@ class Socket {
   constructor() {
     const socket = io(`${serverUrl}/conversations`)
 
-    socket.on('email', email => {
-      this.handlers.forEach(handler => handler(email))
+    socket.on('email', (email) => {
+      this.handlers.forEach((handler) => handler(email))
     })
   }
 
